7250d3280f54bbe161f26eeaec5d41ffeb711199
Marion DEVEAUX <mdeveaux@mailjet.com>
2018-11-12 15:39:57 +0100

    feedback

--- src/styles/AnimatedContainerFlex.js
+++ src/styles/AnimatedContainerFlex.js
@@ -12,21 +12,6 @@ const fade = keyframes`
 `
 
 const AnimatedContainerFlex = styled(ContainerFlex)`
-  display: flex;
-  flex-direction: ${({ fd }) => fd || null};
-  justify-content: ${({ jc }) => jc || null};
-  align-items: ${({ ai }) => ai || null};
-
-  margin-top: ${({ mt, theme }) => theme.spaces[mt]};
-  margin-bottom: ${({ mb, theme }) => theme.spaces[mb]};
-  margin-right: ${({ mr, theme }) => theme.spaces[mr]};
-  margin-left: ${({ ml, theme }) => theme.spaces[ml]};
-
-  background-color: ${({ bc, theme }) => theme.colors[bc] || theme.colors['transparent']};
-
-  width: ${({ w, theme }) => theme.sizes.height[w] || '100%'};
-  height: ${({ h, theme }) => theme.sizes.height[h] || '100%'};
-
   animation: ${fade} 1s;
 `
 
